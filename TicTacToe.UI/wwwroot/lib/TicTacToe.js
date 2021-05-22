a400b0e4cebefd49e8ebdad5637536dce8e7b131
Антон Гребенкин <papavel1997@gmail.com>
2021-05-22 16:12:01 +0700

    .

--- TicTacToe.UI/wwwroot/lib/TicTacToe.js
+++ TicTacToe.UI/wwwroot/lib/TicTacToe.js
@@ -1,56 +1,94 @@
-﻿function ApiClient(url)
-{
-    this.player = "test";
-    this.CreatePlayer = function ()
-    {
-        $.post(url + "/player/create")
-            .done(function (data) {
-                console.log("test");
-                this.player = new Player(data.id, data.name, data.gamesCount, data.winsCount, data.failuresCount);
-                return this.player;
-                //let player = new Player(data.id, data.name, data.gamesCount, data.winsCount, data.failuresCount);
-                //return player;
-                //return new Player(data.id, data.name, data.gamesCount, data.winsCount, data.failuresCount);
-            })
-            .fail(function (data) {
-                throw new ApiException(data.responseJSON.status, data.responseJSON.title)
-            });
-    };
-}
-
-function Player(id, name, gamesCount, winsCount, failuresCount)
-{
-    this.id = id;
-    this.name = name;
-    this.gamesCount = gamesCount;
-    this.winsCount = winsCount;
-    this.failuresCount = failuresCount;
-}
-
-function ApiException(status, title)
-{
-    this.status = status;
-    this.title = title;
-}
-
-console.log("hi");
-$("#test").on("click", function () {
-    console.log("hi2");
-    var client = new ApiClient("http://localhost:21842/api");
-    client.CreatePlayer();
-});
+﻿let playerId;
+let playerPiece;
+let gameId;
 
 const getCookieValue = (name) => (
     document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
 )
 
-$(document).ready(function () {
-    console.log(getCookieValue("playerId"));
-    var apiClient = new ApiClient("http://localhost:21842/api");
-    if (getCookieValue("playerId") == '') {
-        var player = apiClient.CreatePlayer();
-        var player = apiClient.player;
-        document.cookie = "playerId=" + player.id;
-        console.log(document.cookie);
+function changePlayerData(playerId)
+{
+    $.post("http://localhost:21842/api/player/info/" + playerId)
+        .done(function (player)
+        {
+            $("#gamesCount").text(player.gamesCount);
+            $("#winsCount").text(player.winsCount);
+            $("#failuresCount").text(player.failuresCount);
+        })
+        .fail(function (data)
+        {
+            alert("Error:" + data.title);
+        });
+}
+
+function startGame(playerId) {
+    $(".block").empty();
+    $(".block").addClass("block-empty");
+    $.post("http://localhost:21842/api/game/startNew/" + playerId)
+        .done(function (game) {
+            gameId = game.id;
+            playerPiece = game.playerPiece;
+            $("#playerPiece").text(playerPiece);
+            for (let i = 0; i < game.gameBoard.length; i++) {
+                if (game.gameBoard[i] != null) {
+                    $(`#piece-${i}`).text(game.gameBoard[i]);
+                    $(`#piece-${i}`).removeClass("block-empty");
+                }
+            }
+        })
+        .fail(function (data) {
+            alert("Error:" + data.title);
+        });
+}
+
+function makeMove(position) {
+    $.post(`http://localhost:21842/api/game/makeMove/${gameId}/${playerId}/${position}`)
+        .done(function (game) {
+            for (let i = 0; i < game.gameBoard.length; i++) {
+                if (game.gameBoard[i] != null && $(`#piece-${i}`).text() == "") {
+                    $(`#piece-${i}`).text(game.gameBoard[i]);
+                    $(`#piece-${i}`).removeClass("block-empty");
+                }
+            }
+
+            if (game.isCompleted) {
+                changePlayerData(playerId);
+                startGame(playerId);
+            }
+        })
+        .fail(function (data) {
+            alert("Error:" + data.title);
+        });
+}
+
+$(document).ready(function ()
+{
+    playerId = getCookieValue("playerId");
+    if (playerId == '') {
+        $.post("http://localhost:21842/api/player/create")
+            .done(function (player) {
+                document.cookie = "playerId=" + player.id;
+                playerId = player.id;
+                changePlayerData(player.id);
+                startGame(player.id);
+            })
+            .fail(function (data) {
+                alert("Error:" + data.title);
+            });
+    } else {
+        changePlayerData(playerId);
+        startGame(playerId);
+    }  
+});
+
+$(".block").on("click", function () {
+    if ($(this).text() == "") {
+        $(this).text(playerPiece);
+        $(this).removeClass("block-empty");
+        makeMove(this.id.substring(6))
     }
 });
+
+$("#resetGame").on("click", function () {
+    startGame(playerId);
+})
